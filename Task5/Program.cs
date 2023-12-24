880d64abf09c7d287c9f781bf3c4c4195062086a
ESvLana <Sv9toshka@mail.ru>
2023-12-24 03:33:11 +0300

    add Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿int N = 100;
+int i = 1;
+
+while (i <= N)
+{
+    // Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(',');
+    Console.Write(' ');
+    i = i + 1;
+}
+
+// Console.WriteLine("Hello, World!");
