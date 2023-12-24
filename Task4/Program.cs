413efa3cb4cd7ac6ef5fba170a0a6273c3980fdc
ESvLana <Sv9toshka@mail.ru>
2023-12-24 03:09:41 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 631;
+int b = 7;
+int c = 99;
+int d = 1;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
